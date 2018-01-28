c26bb2d0ff396549b085a743a63b83ddf59a29bd
by student <studentsctest@gmail.com>
2018-01-29 08:59:08 +0900

    XPS and UPS template

---
+++ XPS_and_UPS/UPS_and_XPS(VB)_TNR/UPS_and_XPS(VB)_TNR.bat
@@ -0,0 +1,240 @@
+@echo off
+rem gnuplot version 5.2
+
+rem setting gnuplot address (default)
+ set gplot="C:\Program Files\gnuplot\bin"
+ set int_offset=100
+ set int_base=0
+ rem set font=Arial
+ set font=Times-New-Roman
+
+rem composition
+ set compsition=""
+ set label1_compsition=""
+
+rem plot number of column for x and y axis
+ set x=1
+ set y=2
+
+rem line wide and point size
+ set line_wide=2
+ set point_size=2
+rem gnuplot# reccommend 2
+rem eps# recommend line_wide=8
+rem eps# recommend point_size=3
+
+rem line type
+ set line_type_data1=7
+ set line_type_data2=7
+ set line_type_data3=7
+ set line_type_data4=7
+ set line_type_data5=7
+ set line_type_data6=7
+rem gnuplot#I recommend 7, 0
+rem eps#I recommend -1 to 5
+
+rem setting notation
+ set name_data1=""
+ set name_data2=""
+ set name_data3=""
+ set name_data4=""
+ set name_data5=""
+ set name_data6=""
+rem #nothing case = ""
+
+rem number of marker type
+ set marker_data1=7
+ set marker_data2=7
+ set marker_data3=7
+ set marker_data4=7
+ set marker_data5=7
+ set marker_data6=7
+
+rem marker type info for gnuplot
+rem gnuplot#circle  : 7
+rem gnuplot#square  : 5
+rem gnuplot#upper triangular: 9
+rem gnuplot#lower triangular: 11
+rem gnuplot#pentagon: 15
+rem gnuplot#diamond : 13
+rem gnuplot#cross: 2
+
+rem marker type info for eps
+rem eps#close circle  : 31, open circle : 65
+rem eps#close square  : 47, open square : 64
+rem eps#close upper triangular:9  ,open upper triangular: 66
+rem eps#close lower triangular:11 ,open lower triangular: 67
+rem eps#close pentagon: 15, open pentagon: 75
+rem eps#close diamond : 13, open diamond: 68
+rem eps#cross: 2
+rem eps#open maker: 64-75
+
+rem number of color type
+ set color_data1=black
+ set color_data2=black
+ set color_data3=black
+ set color_data4=black
+ set color_data5=black
+ set color_data6=black
+rem red, dark-goldenrod, dark-green, blue, dark-violet, black, grey
+rem grey, black, blue, dark-green, dark-goldenrod, red, dark-violet
+
+rem use wilde card
+if exist *.dat2 (goto data3)
+ copy *.dat1 data.dat1
+ goto start
+:data3
+if exist *.dat3 (goto data4)
+ copy *.dat1 data.dat1
+ copy *.dat2 data.dat2
+ goto start
+:data4
+if exist *.dat4 (goto data5)
+ copy *.dat1 data.dat1
+ copy *.dat2 data.dat2
+ copy *.dat3 data.dat3
+ goto start
+:data5
+if exist *.dat5 (goto data6)
+ copy *.dat1 data.dat1
+ copy *.dat2 data.dat2
+ copy *.dat3 data.dat3
+ copy *.dat4 data.dat4
+ goto start
+:data6
+if exist *.dat6 (goto data7)
+ copy *.dat1 data.dat1
+ copy *.dat2 data.dat2
+ copy *.dat3 data.dat3
+ copy *.dat4 data.dat4
+ copy *.dat5 data.dat5
+ goto start
+:data7
+if exist *.dat7 (goto data8)
+ copy *.dat1 data.dat1
+ copy *.dat2 data.dat2
+ copy *.dat3 data.dat3
+ copy *.dat4 data.dat4
+ copy *.dat5 data.dat5
+ copy *.dat6 data.dat6
+ goto start
+:data8
+:start
+
+if not exist env.txt (goto nofile) else (goto file_exsit)
+
+:nofile
+rem graph environment 
+ echo ########################				> env.txt
+ echo #graph setting 					>> env.txt
+ echo set size 0.54,1.0					>> env.txt
+ echo set key title %compsition%			>> env.txt
+ echo set key right top					>> env.txt
+ echo set label 1 at graph 0.50,0.93 %label1_compsition%	>> env.txt
+ echo set terminal wxt font "%font%,14"			>> env.txt
+ echo set mxtics 2					>> env.txt
+ echo set xtics 1 offset 0.0,0.25			>> env.txt
+ echo set xlabel "Binding Energy / eV" offset 0.0,0.70	>> env.txt
+ echo set xrange[*:*] reverse					>> env.txt
+ echo set noytics					>> env.txt
+ echo set yrange[0:800]		>> env.txt
+ echo set format y ""					>> env.txt
+ echo set ylabel "Intensity / arb. units" offset 0.0,0.0	>> env.txt
+
+rem #search number of row in file 
+rem for /f "delims=:" %%A in ('findstr /n "^" "data.dat1"') do set LINE=%%A-1
+rem echo set sample %LINE%					>> env.txt
+
+if exist *.dat2 (goto next3)
+ echo plot "data.dat1" u %x%:($%y%+%int_base%) t %name_data1% w l lt %line_type_data1% lw %line_wide% lc "%color_data1%" >> env.txt
+ goto end
+
+:next3
+if exist *.dat3 (goto next4)
+ echo plot "data.dat1" u %x%:($%y%+%int_base%) t %name_data1% w l lt %line_type_data1% lw %line_wide% lc "%color_data1%",^
+  "data.dat2" u %x%:($%y%+%int_base%+%int_offset%*1) t %name_data2% w l lt %line_type_data2% lw %line_wide% lc "%color_data2%"  >> env.txt
+ goto end
+
+:next4
+if exist *.dat4 (goto next5)
+ echo plot "data.dat1" u %x%:($%y%+%int_base%) t %name_data1% w l lt %line_type_data1% lw %line_wide% lc "%color_data1%",^
+  "data.dat2" u %x%:($%y%+%int_base%+%int_offset%*1) t %name_data2% w l lt %line_type_data2% lw %line_wide% lc "%color_data2%",^
+  "data.dat3" u %x%:($%y%+%int_base%+%int_offset%*2) t %name_data3% w l lt %line_type_data3% lw %line_wide% lc "%color_data3%" 	>> env.txt
+ goto end
+
+:next5
+if exist *.dat5 (goto next6)
+ echo plot "data.dat1" u %x%:($%y%+%int_base%) t %name_data1% w l lt %line_type_data1% lw %line_wide% lc "%color_data1%",^
+  "data.dat2" u %x%:($%y%+%int_base%+%int_offset%*1) t %name_data2% w l lt %line_type_data2% lw %line_wide% lc "%color_data2%",^
+  "data.dat3" u %x%:($%y%+%int_base%+%int_offset%*2) t %name_data3% w l lt %line_type_data3% lw %line_wide% lc "%color_data3%",^
+  "data.dat4" u %x%:($%y%+%int_base%+%int_offset%*3) t %name_data4% w l lt %line_type_data4% lw %line_wide% lc "%color_data4%"	>> env.txt
+ goto end
+
+:next6
+if exist *.dat6 (goto next7)
+ echo plot "data.dat1" u %x%:($%y%+%int_base%) t %name_data1% w l lt %line_type_data1% lw %line_wide% lc "%color_data1%",^
+  "data.dat2" u %x%:($%y%+%int_base%+%int_offset%*1) t %name_data2% w l lt %line_type_data2% lw %line_wide% lc "%color_data2%",^
+  "data.dat3" u %x%:($%y%+%int_base%+%int_offset%*2) t %name_data3% w l lt %line_type_data3% lw %line_wide% lc "%color_data3%",^
+  "data.dat4" u %x%:($%y%+%int_base%+%int_offset%*3) t %name_data4% w l lt %line_type_data4% lw %line_wide% lc "%color_data4%",^
+  "data.dat5" u %x%:($%y%+%int_base%+%int_offset%*4) t %name_data5% w l lt %line_type_data5% lw %line_wide% lc "%color_data5%" >> env.txt
+ goto end
+
+:next7
+if exist *.dat7 (goto next8)
+ echo plot "data.dat1" u %x%:($%y%+%int_base%) t %name_data1% w l lt %line_type_data1% lw %line_wide% lc "%color_data1%",^
+  "data.dat2" u %x%:($%y%+%int_base%+%int_offset%*1) t %name_data2% w l lt %line_type_data2% lw %line_wide% lc "%color_data2%",^
+  "data.dat3" u %x%:($%y%+%int_base%+%int_offset%*2) t %name_data3% w l lt %line_type_data3% lw %line_wide% lc "%color_data3%",^
+  "data.dat4" u %x%:($%y%+%int_base%+%int_offset%*3) t %name_data4% w l lt %line_type_data4% lw %line_wide% lc "%color_data4%",^
+  "data.dat5" u %x%:($%y%+%int_base%+%int_offset%*4) t %name_data5% w l lt %line_type_data5% lw %line_wide% lc "%color_data5%",^
+  "data.dat6" u %x%:($%y%+%int_base%+%int_offset%*5) t %name_data6% w l lt %line_type_data6% lw %line_wide% lc "%color_data6%" 	>> env.txt
+ goto end
+:next8
+:end
+
+ echo ########################	>> env.txt
+ echo #another setting and info	>> env.txt
+ echo # w=with, lp=linespoints,lt=linetypeClw=linewidthCpt=pointtypeCps=pointsize	>> env.txt
+ echo #				>> env.txt
+ echo # offset 1000		>> env.txt
+ echo # echo plot "data.dat1" u 1:($2+1000) t "" w l lt 7 lw 2 lc "black" 	>> env.txt
+ echo #				>> env.txt
+ echo # plot "data.dat1" every 20 using 1:2 smooth csplines t "" w lp lt 7 lw 2 pt 7 ps 2 lc "black" 	>> env.txt
+
+rem output (png)
+rem echo ########################	>> env.txt
+rem echo #png setting 		>> env.txt
+rem echo set terminal pngcairo enhanced  		>> env.txt
+rem echo set output "kappa_vs_com_1f_JIM_Arial.png"	>> env.txt
+rem echo replot			>> env.txt
+rem echo set output		>> env.txt
+rem echo set terminal wxt		>> env.txt
+
+rem output (EPS)
+rem echo ########################	>> env.txt
+rem echo #EPS setting 		>> env.txt
+rem echo set size 0.7,1.0		>> env.txt
+rem echo set terminal postscript eps color enhanced font "%font%" 22	>> env.txt
+rem echo set output "kappa_vs_com_1f_JIM_Arial.eps"	>> env.txt
+rem echo replot			>> env.txt
+rem echo set output		>> env.txt
+rem echo set terminal wxt		>> env.txt
+
+rem aip latex template
+ echo ########################	>> env.txt
+ echo #AIP latex setting	>> env.txt
+ echo # latex template: aipsamp.tex	>> env.txt
+ echo # \includegraphics[width=17cm, bb=0 5 520 185]{graph.eps}% Here is how to import EPS art	>> env.txt
+
+:file_exsit
+
+rem plot
+ %gplot%\wgnuplot.exe -persist "env.txt"
+
+rem delet
+ del data.dat1
+ del data.dat2
+ del data.dat3
+ del data.dat4
+ del data.dat5
+ del data.dat6
+ del env.txt
---
+++ XPS_and_UPS/UPS_and_XPS(VB)_shirley_Arial/UPS_and_XPS(VB)_Shirley.bat
@@ -0,0 +1,68 @@
+@echo off
+rem gnuplot version 5.2
+
+rem setting gnuplot address (default)
+ set gplot="C:\Program Files\gnuplot\bin"
+ set font=Arial
+ rem set font=Times-New-Roman
+
+rem use wilde card
+ copy *.dat1 data.dat1
+
+if not exist env.txt (goto nofile) else (goto file_exsit)
+
+:nofile
+rem graph environment 
+ echo ########################				> env.txt
+ echo #graph setting 					>> env.txt
+ echo set size 0.70,1.0					>> env.txt
+ echo set terminal wxt font "%font%,14"			>> env.txt
+ echo set mxtics 2					>> env.txt
+ echo set xtics 1 offset 0.0,0.25			>> env.txt
+ echo set xlabel "Binding Energy / eV" offset 0.0,0.70	>> env.txt
+ echo set xrange[*:*] reverse				>> env.txt
+ echo set mytics 2					>> env.txt
+ echo set ytics 50 offset 0.3,0.0			>> env.txt
+ echo set ylabel "Intensity / arb. units" offset 1.5,0.0	>> env.txt
+ echo plot "data.dat1" u 1:2 t "" w lp lt 7 lw 2 pt 7 ps 2 lc "black", ^
+  "data.dat1" u 1:3 t "" w lp lt 2 lw 7 pt -1 ps 2 lc "gray",^
+  "data.dat1" u 1:($2-$3) t "" w lp lt 7 lw 7 pt -1 ps 2 lc "black"	>> env.txt
+
+ echo ########################	>> env.txt
+ echo #another setting and info	>> env.txt
+ echo # w=with, lp=linespoints,lt=linetypeClw=linewidthCpt=pointtypeCps=pointsize	>> env.txt
+ echo # plot "data.dat1" every 2 using 1:2 smooth csplines t "" w lp lt 7 lw 2 pt 7 ps 2 lc "black" 	>> env.txt
+
+rem output (png)
+rem echo ########################	>> env.txt
+rem echo #png setting 		>> env.txt
+rem echo set terminal pngcairo enhanced  		>> env.txt
+rem echo set output "kappa_vs_com_1f_JIM_Arial.png"	>> env.txt
+rem echo replot			>> env.txt
+rem echo set output		>> env.txt
+rem echo set terminal wxt		>> env.txt
+
+rem output (EPS)
+rem echo ########################	>> env.txt
+rem echo #EPS setting 		>> env.txt
+rem echo set size 0.7,1.0		>> env.txt
+rem echo set terminal postscript eps color enhanced font "%font%" 22	>> env.txt
+rem echo set output "kappa_vs_com_1f_JIM_Arial.eps"	>> env.txt
+rem echo replot			>> env.txt
+rem echo set output		>> env.txt
+rem echo set terminal wxt		>> env.txt
+
+rem aip latex template
+ echo ########################	>> env.txt
+ echo #AIP latex setting	>> env.txt
+ echo # latex template: aipsamp.tex	>> env.txt
+ echo # \includegraphics[width=17cm, bb=0 5 520 185]{graph.eps}% Here is how to import EPS art	>> env.txt
+
+:file_exsit
+
+rem plot
+ %gplot%\wgnuplot.exe -persist "env.txt"
+
+rem delet
+ del data.dat1
+ del env.txt
